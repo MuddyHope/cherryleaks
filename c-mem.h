275340bbcd584102b516c2e496996f34ce74b3c8
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-06-05 11:47:10 +0300

    dma-func: fix typos & remove unneeded code

--- c-mem.h
+++ c-mem.h
@@ -32,8 +32,6 @@ list* GLOBAL_LIST = NULL;
  */
 #define END_PRINT_ALL
 
-
-#define test if(list_is_null(GLOBAL_LIST)) printf("Inside\n")
 #define malloc(n) list_is_null(GLOBAL_LIST) ? memory_data_malloc(n, __FILE__, __LINE__, GLOBAL_LIST) : malloc(n)
 #define realloc(ptr, n) list_is_null(GLOBAL_LIST) ? memory_data_realloc(ptr, n __FILE__, __LINE__, GLOBAL_LIST) : realloc(ptr, n)
 #define calloc(n, size) list_is_null(GLOBAL_LIST) ? memory_data_calloc(n, size, __FILE__, __LINE__, GLOBAL_LIST) : calloc(n, size)
--- inc/c-mem-dma-func.h
+++ inc/c-mem-dma-func.h
@@ -7,9 +7,9 @@
 /** Add data to mem-block entity
  * 
  * @param amount - amount of bytes to be allocated
- * @param file  - name of teh file where allocation is done
+ * @param file  - name of the file where allocation is done
  * @param line - line number where allocation is done
- * @param list - gobal list structure where memory data is stored 
+ * @param list - global list structure where memory data is stored 
  * @return pointer to allocated memory
  */
 void* memory_data_malloc(size_t amount, char* file, size_t line, list *l);
@@ -19,9 +19,9 @@ void* memory_data_malloc(size_t amount, char* file, size_t line, list *l);
  * 
  * @param ptr   - This is the pointer to a memory block previously allocated with malloc, calloc or realloc to be reallocated. 
  * @param amount - the amount of bytes to be reallocated
- * @param file  - name of teh file where allocation is done
+ * @param file  - name of the file where allocation is done
  * @param line - line number where allocation is done
- * @param list - gobal list structure where memory data is stored 
+ * @param list - global list structure where memory data is stored 
  * @return pointer to reallocated memory 
  */
 void* memory_data_realloc(void* ptr, size_t amount, char* file, size_t line, list *l);
@@ -31,9 +31,9 @@ void* memory_data_realloc(void* ptr, size_t amount, char* file, size_t line, lis
  * 
  * @param amount - amount of data-units to be allocated
  * @param size - size of a data-unit
- * @param file  - name of teh file where allocation is done
+ * @param file  - name of the file where allocation is done
  * @param line - line number where allocation is done
- * @param list - gobal list structure where memory data is stored 
+ * @param list - global list structure where memory data is stored 
  * @return pointer to allocated memory
  */
 void* memory_data_calloc(size_t amount, size_t size, char* file, size_t line, list* l);
@@ -42,9 +42,9 @@ void* memory_data_calloc(size_t amount, size_t size, char* file, size_t line, li
  *
  * 
  * @param ptr - pointer to allocated memory
- * @param file  - name of teh file where allocation is done
+ * @param file  - name of the file where allocation is done
  * @param line - line number where allocation is done
- * @param list - gobal list structure where memory data is stored 
+ * @param list - global list structure where memory data is stored 
  */
 void memory_data_free(void* ptr, char* file, size_t line, list* l);
 
