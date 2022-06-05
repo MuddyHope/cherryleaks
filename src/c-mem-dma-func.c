ddf9e687b6640e96a16dedd008d80b415ee03905
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-06-05 11:53:27 +0300

    dma-func: create memory_data_calloc functionality

--- src/c-mem-dma-func.c
+++ src/c-mem-dma-func.c
@@ -16,6 +16,17 @@ void* memory_data_malloc(size_t amount, char* file, size_t line, list *l)
 
 void* memory_data_realloc(void* ptr, size_t amount, char* file, size_t line, list *l){}
 
-void* memory_data_calloc(size_t amount, size_t size, char* file, size_t line, list* l){}
+void* memory_data_calloc(size_t amount, size_t size, char* file, size_t line, list* l)
+{
+    void* alloc_addr = calloc(amount, size);
+    c_mem_entity block;
+
+    block.address = alloc_addr;
+    block.size = amount*size;
+    block.file = file;
+    block.line = line;
+    list_push_back(l, (void*)&block, sizeof(block));
+    return alloc_addr;
+}
 
 void memory_data_free(void* ptr, char* file, size_t line, list* l){}
