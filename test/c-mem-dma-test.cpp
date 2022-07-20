e4a8aaa8bd1835344046328f3571bc58c88ad0cf
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-07-20 19:10:14 +0300

    c-mem-tests: add basic dma tests
    These tests are testing basic functionality, such as vreating the c-mem entity and values inside

--- test/c-mem-dma-test.cpp
+++ test/c-mem-dma-test.cpp
@@ -9,25 +9,57 @@ extern "C" {
 TEST(c_mem_dma_test, test_malloc_struct_internal) {
     C_MEM_START
     c_mem_entity *buffer;
-    void *pointer = malloc(100);
+    void *pointer = malloc(100); int line = __LINE__;
     buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
     EXPECT_EQ(buffer->address, pointer);
     EXPECT_EQ(buffer->alloc_type, MALLOCATED);
     EXPECT_EQ(buffer->size, 100);
-    C_MEM_END_PRINT_LEFT //TODO: Make a separate version to delete a list
+    EXPECT_EQ(buffer->line, line);
+    EXPECT_EQ(buffer->file, __FILE__);
+    list_clear(GLOBAL_LIST);
     free(pointer);
 }
 
 TEST(c_mem_dma_test, test_realloc_struct_internal) {
-    ASSERT_EQ(0, 0);
+    C_MEM_START
+    c_mem_entity *buffer;
+    void *pointer = realloc(NULL, 100); int line = __LINE__;
+    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
+    EXPECT_EQ(buffer->address, pointer);
+    EXPECT_EQ(buffer->alloc_type, REALLOACTED);
+    EXPECT_EQ(buffer->size, 100);
+    EXPECT_EQ(buffer->line, line);
+    EXPECT_EQ(buffer->file, __FILE__);
+    list_clear(GLOBAL_LIST);
+    free(pointer);
 }
 
 TEST(c_mem_dma_test, test_calloc_struct_internal) {
-    ASSERT_EQ(0, 0);
+    C_MEM_START
+    c_mem_entity *buffer;
+    void *pointer = calloc(100, 1); int line = __LINE__;
+    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
+    EXPECT_EQ(buffer->address, pointer);
+    EXPECT_EQ(buffer->alloc_type, CALLOCATED);
+    EXPECT_EQ(buffer->size, 100);
+    EXPECT_EQ(buffer->line, line);
+    EXPECT_EQ(buffer->file, __FILE__);
+    list_clear(GLOBAL_LIST);
+    free(pointer);
 }
 
-TEST(c_mem_dma_test, test_free) {
-    ASSERT_EQ(0, 0);
+TEST(c_mem_dma_test, test_free_struct_internal) {
+    C_MEM_START
+    c_mem_entity *buffer;
+    void * pointer = malloc(100);
+    free(pointer); int line = __LINE__;
+    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
+    EXPECT_EQ(buffer->address, pointer);
+    EXPECT_EQ(buffer->alloc_type, FREED);
+    EXPECT_EQ(buffer->size, 0);
+    EXPECT_EQ(buffer->line, line);
+    EXPECT_EQ(buffer->file, __FILE__);
+    list_clear(GLOBAL_LIST);
 }
 
 int main(int argc, char **argv) {
