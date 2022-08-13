b5de43adfbfa8ee00ccd540b6b3fe830fe74abdd
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-08-13 12:32:55 +0300

    test: clear old tests

--- test/cMemDmaTest.cpp
+++ test/cMemDmaTest.cpp
@@ -21,93 +21,35 @@ extern "C" {
 }
 /** MEMORY_DATA_MALLOC */
 TEST(cMemDmaTest, test_malloc_struct_internal) {
-  C_MEM_START
-    c_mem_entity *buffer;
-  void *pointer = malloc(100); int line = __LINE__;
-  buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-  EXPECT_EQ(buffer->address, pointer);
-  EXPECT_EQ(buffer->alloc_type, MALLOCATED);
-  EXPECT_EQ(buffer->size, 100);
-  EXPECT_EQ(buffer->line, line);
-  EXPECT_EQ(buffer->file, __FILE__);
-  list_clear(GLOBAL_LIST);
-  free(pointer);
+
 }
 
 TEST(cMemDmaTest, test_malloc_return) {
-    C_MEM_START
-     c_mem_entity *buffer;
-    void *pointer = malloc(100);
-    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-    ASSERT_EQ(pointer, buffer->address);
-    list_clear(GLOBAL_LIST);
-    free(pointer);
+
 }
 
 /** MEMORY_DATA_REALLOC */
 TEST(cMemDmaTest, test_realloc_struct_internal) {
-  C_MEM_START
-    c_mem_entity *buffer;
-  void *pointer = realloc(NULL, 100); int line = __LINE__;
-  buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-  EXPECT_EQ(buffer->address, pointer);
-  EXPECT_EQ(buffer->alloc_type, REALLOACTED);
-  EXPECT_EQ(buffer->size, 100);
-  EXPECT_EQ(buffer->line, line);
-  EXPECT_EQ(buffer->file, __FILE__);
-  list_clear(GLOBAL_LIST);
-  free(pointer);
+
 }
 
 TEST(cMemDmaTest, test_realloc_return) {
-    C_MEM_START
-    c_mem_entity *buffer;
-    void *pointer = realloc(nullptr, 100);
-    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-    ASSERT_EQ(pointer, buffer->address);
-    list_clear(GLOBAL_LIST);
-    free(pointer);
+
 }
 
 
 /** MEMORY_DATA_CALLOC */
 TEST(cMemDmaTest, test_calloc_struct_internal) {
-  C_MEM_START
-    c_mem_entity *buffer;
-  void *pointer = calloc(100, 1); int line = __LINE__;
-  buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-  EXPECT_EQ(buffer->address, pointer);
-  EXPECT_EQ(buffer->alloc_type, CALLOCATED);
-  EXPECT_EQ(buffer->size, 100);
-  EXPECT_EQ(buffer->line, line);
-  EXPECT_EQ(buffer->file, __FILE__);
-  list_clear(GLOBAL_LIST);
-  free(pointer);
+
 }
 
 TEST(cMemDmaTest, test_calloc_return) {
-    C_MEM_START
-    c_mem_entity *buffer;
-    void *pointer = calloc(1, 100);
-    buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-    ASSERT_EQ(pointer, buffer->address);
-    list_clear(GLOBAL_LIST);
-    free(pointer);
+
 }
 
 /** MEMORY_DATA_FREE */
 TEST(cMemDmaTest, test_free_struct_internal) {
-  C_MEM_START
-    c_mem_entity *buffer;
-  void *pointer = malloc(100);
-  free(pointer); int line = __LINE__;
-  buffer = ((c_mem_entity *)GLOBAL_LIST->first->content);
-  EXPECT_EQ(buffer->address, pointer);
-  EXPECT_EQ(buffer->alloc_type, FREED);
-  EXPECT_EQ(buffer->size, 0);
-  EXPECT_EQ(buffer->line, line);
-  EXPECT_EQ(buffer->file, __FILE__);
-  list_clear(GLOBAL_LIST);
+
 }
 
 int main(int argc, char **argv) {
